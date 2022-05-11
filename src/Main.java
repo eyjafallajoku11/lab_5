7dcc0dcec1744003d9c761d46409ed49a3b4455b
eyjafallajoku11 <47786206+eyjafallajoku11@users.noreply.github.com>
2022-05-11 10:08:20 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-import utility.command_info;
-import utility.command_manager;
+import utility.*;
+import utility.bareCommand.*;
+import utility.creatorCommand.CreatorCommandInsert;
+import utility.creatorCommand.CreatorCommandRemoveLower;
+import utility.creatorCommand.CreatorCommandReplaceIfGreater;
+import utility.creatorCommand.CreatorCommandUpdate;
+import utility.dataCommand.*;
+import utility.humanCommand.HumanCommandFilterLessThanGovernor;
+
 import java.io.BufferedInputStream;
 import java.io.IOException;
 
@@ -36,13 +43,31 @@ public class Main {
             return null;
         }
     }
-
     public static void main(String[] arguments) {
-        command_info.initialise();
-        while(true) {
-           String[] input = readLine();
-           command_manager.execute(input);
+        CommandManager.bareRegister("help", new BareCommandHelp());
+        CommandManager.bareRegister("exit", new BareCommandExit());
+        CommandManager.bareRegister("show", new BareCommandShow());
+        CommandManager.bareRegister("clear", new BareCommandClear());
+        CommandManager.bareRegister("print_descending", new BareCommandPrintDescending());
+        CommandManager.bareRegister("history", new BareCommandHistory());
+        CommandManager.bareRegister("info", new BareCommandInfo());
 
-       }
+        CommandManager.creatorRegister("remove_lower", new CreatorCommandRemoveLower());
+        CommandManager.creatorRegister("insert", new CreatorCommandInsert());
+        CommandManager.creatorRegister("update", new CreatorCommandUpdate());
+        CommandManager.creatorRegister("replace_if_greater", new CreatorCommandReplaceIfGreater());
+
+        CommandManager.dataRegister("execute_script", new DataCommandExecuteScript());
+        CommandManager.dataRegister("save", new DataCommandSave());
+        CommandManager.dataRegister("open", new DataCommandOpenFile());
+        CommandManager.dataRegister("remove_key", new DataCommandRemoveKey());
+        CommandManager.dataRegister("remove_all_by_government", new DataCommandRemoveAllByGovernment());
+
+        CommandManager.humanRegister("filter_less_than_governor", new HumanCommandFilterLessThanGovernor());
+
+        while (true) {
+            String[] t = readLine();
+            CommandManager.execute(t[0],t[1]);
+        }
     }
 }
