c86afb30247949a10d0474f04df4b895a202e98d
eyjafallajoku11 <47786206+eyjafallajoku11@users.noreply.github.com>
2022-05-11 10:16:04 +0300

    Delete command_update.java

--- src/utility/command_update.java
+++
@@ -1,8 +0,0 @@
-package utility;
-
-public class command_update {
-    public static void execute(String data) {
-        command_remove_key.execute(data);
-        command_insert.execute(data);
-    }
-}
